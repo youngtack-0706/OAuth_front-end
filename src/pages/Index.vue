9f0ff8a9027d697c0618e5d36925b4e5adfadd5e
김 영택 <ytkim@onec.kr>
2021-06-07 17:49:40 +0900

    modify index page

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,32 +1,31 @@
 <template>
   <div>
-    유저 이름:{{$store.state.user.nickname}}<br>
-    유저 메일:{{$store.state.user.email}}<br>
-    <b>count : {{this.$store.state.count}}</b><br>
-    <input type="button" @click="increment()" value="increment"/>
-    <input type="button" @click="decrement()" value="decrement"/>
+      유저 이름:{{$store.state.user.nickname}}<br>
+      유저 메일:{{$store.state.user.email}}<br>
+      유저 생일:{{$store.state.user.birthday}}<br>
+      유저 성별:{{$store.state.user.gender ==='M'?'남자': '여자'}}<br>
+      <b>count : {{this.$store.state.count}}</b><br>
+      <input type="button" @click="increment()" value="increment"/>
+      <input type="button" @click="decrement()" value="decrement"/>
   </div>
 </template>
-
 <script>
-// import {store} from '../store/store'
+  export default {
+    data(){
+      return{
 
-export default ({
-  data(){
-    return{
-
-    }
-  },
-  mounted(){
-    console.log(this.$store.state.user)
-  },
-  methods: {
-    increment: function () {
-      this.$store.commit('increment')
+      }
     },
-    decrement: function () {
-      this.$store.commit('decrement')
+    mounted(){
+
+    },
+    methods:{
+      async increment() {
+       this.$store.commit('increment')
+      },
+      async decrement() {
+        this.$store.commit('decrement')
+      }
     }
   }
-});
 </script>
